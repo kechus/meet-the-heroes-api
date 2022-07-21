14555196c3a0f37bd1a17ea0fc35365476f281af
kechus <kechus4556@gmail.com>
2022-07-21 17:08:04 -0500

    fix docs

--- src/tests/country.test.ts
+++ src/tests/country.test.ts
@@ -2,14 +2,12 @@ import chai, { expect } from 'chai';
 	import chaiAsPromised from 'chai-as-promised';
 	import chaiHttp from 'chai-http';
 import { it, describe } from 'mocha';
-import { Prueba } from '@models/Prueba';
 
 chai.use(chaiAsPromised);
 chai.use(chaiHttp);
 
 import { createApp } from '../app';
 import { Country } from '@models/Country';
-import exp from 'constants';
 
 describe('Countries', () => {
 	let app: any;
@@ -20,12 +18,12 @@ describe('Countries', () => {
 
 	it('should read from db', async ()=>{
 		const countries = await Country.findAll();
-        expect(countries.length).to.eql(21)
+        expect(countries.length).to.eql(920)
 	})
 
 	it('should test endpoint', async ()=>{
 		const response = await chai.request(app).get(`/api/v1/countries`).send();
 		expect(response).have.status(200);
-        expect(response.body.length).to.eql(21)
+        expect(response.body.length).to.eql(920)
 	})
 });
